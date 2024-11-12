2d0a976f79869bdf3b1ac566e399c8357a6ce4e1
3090177148 <3090177148@qq.com>
2024-11-12 10:15:39 +0800

    Update jp.js

--- jp.js
+++ jp.js
@@ -1,150 +1,150 @@
 /**
-*影视电视在你吗
-* https://t.me/fongmi_offical/
-* https://github.com/FongMi/Release/tree/main/apk
+ * 影视TV 超連結跳轉支持
+ * https://t.me/fongmi_offical/
+ * https://github.com/FongMi/Release/tree/main/apk
  */
 
-哪里 规则 ={
-	标题: '荐片',
-"地區"
-查询为空
-查询为空
-查询为空
-查询为空
-查询为空
-查询为空
-查询为空
-查询为空
-查询为空
-查询为空
-查询为空
-查询为空
-,
-,
-查询为空
-		"4":[{"key":"cateId","name":"全部","value":[{"n":"国产","v":"4"},{"n":"中国香港","v":"23"},{"n":"中国台湾","v":"24"},{"n":"美国","v":"25"},{"n":"韩国","v":"26"}]},{"key":"area","name":"日本","value":[{"n":"年代","v":"0"},{"n":"全部","v":"1"},{"n":"排序","v":"3"},{"n":"热门","v":"6"},{"n":"评分","v":"5"},{"n":abcD (a + b + c + D) (abcD (a + b + c + D) (abc))def ghi jkl mno pqr stu,"v":"18"},{"n":'JPAUTH': 'y261 ow 7 kF 2 dtzlxh1 GS 9 EB8 nbTxNmaK编者某偃汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲
+var rule = {
+	title: '荐片',
+	host: 'http://api2.rinhome.com',
+	homeUrl: '/api/tag/hand?code=unknown601193cf375db73d&channel=wandoujia',//网站的首页链接,用于分类获取和推荐获取
+	// url:'/api/crumb/list?area=0&category_id=fyclass&page=fypage&type=0&limit=24&fyfilter',
+	url: '/api/crumb/list?page=fypage&type=0&limit=24&fyfilter',
+	class_name: '全部&电影&电视剧&动漫&综艺',     // 筛选 /api/term/ad_fenlei?limit=10&page=1
+	class_url: '0&1&2&3&4',
+	detailUrl: '/api/node/detail?channel=wandoujia&token=&id=fyid',//二级详情拼接链接(json格式用)
+	searchUrl: '/api/video/search?key=**&page=fypage',
+	searchable: 2,
+	quickSearch: 0,
+	filterable: 1,
+	filter: {
+		"0":[{"key":"area","name":"地區","value":[{"n":"全部","v":"0"},{"n":"国产","v":"1"},{"n":"中国香港","v":"3"},{"n":"中国台湾","v":"6"},{"n":"美国","v":"5"},{"n":"韩国","v":"18"},{"n":"日本","v":"2"}]},{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"n":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","v":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}],
+		"1":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"1"},{"n":"首推","v":"5"},{"n":"动作","v":"6"},{"n":"喜剧","v":"7"},{"n":"战争","v":"8"},{"n":"恐怖","v":"9"},{"n":"剧情","v":"10"},{"n":"爱情","v":"11"},{"n":"科幻","v":"12"},{"n":"动画","v":"13"}]},{"key":"area","name":"地區","value":[{"n":"全部","v":"0"},{"n":"国产","v":"1"},{"n":"中国香港","v":"3"},{"n":"中国台湾","v":"6"},{"n":"美国","v":"5"},{"n":"韩国","v":"18"},{"n":"日本","v":"2"}]},{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"n":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","v":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}],
+		"2":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"2"},{"n":"首推","v":"14"},{"n":"国产","v":"15"},{"n":"港台","v":"16"},{"n":"日韩","v":"17"},{"n":"海外","v":"18"}]},{"key":"area","name":"地區","value":[{"n":"全部","v":"0"},{"n":"国产","v":"1"},{"n":"中国香港","v":"3"},{"n":"中国台湾","v":"6"},{"n":"美国","v":"5"},{"n":"韩国","v":"18"},{"n":"日本","v":"2"}]},{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"n":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","v":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}],
+		"3":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"3"},{"n":"首推","v":"19"},{"n":"海外","v":"20"},{"n":"日本","v":"21"},{"n":"国产","v":"22"}]},{"key":"area","name":"地區","value":[{"n":"全部","v":"0"},{"n":"国产","v":"1"},{"n":"中国香港","v":"3"},{"n":"中国台湾","v":"6"},{"n":"美国","v":"5"},{"n":"韩国","v":"18"},{"n":"日本","v":"2"}]},{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"n":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","v":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}],
+		"4":[{"key":"cateId","name":"分类","value":[{"n":"全部","v":"4"},{"n":"首推","v":"23"},{"n":"国产","v":"24"},{"n":"海外","v":"25"},{"n":"港台","v":"26"}]},{"key":"area","name":"地區","value":[{"n":"全部","v":"0"},{"n":"国产","v":"1"},{"n":"中国香港","v":"3"},{"n":"中国台湾","v":"6"},{"n":"美国","v":"5"},{"n":"韩国","v":"18"},{"n":"日本","v":"2"}]},{"key":"year","name":"年代","value":[{"n":"全部","v":"0"},{"n":"2023","v":"153"},{"n":"2022","v":"101"},{"n":"2021","v":"118"},{"n":"2020","v":"16"},{"n":"2019","v":"7"},{"n":"2018","v":"2"},{"n":"2017","v":"3"},{"n":"2016","v":"22"}]},{"key":"sort","name":"排序","value":[{"n":"热门","v":"hot"},{"n":"评分","v":"rating"},{"n":"更新","v":"update"}]}]
 	},
-	filter_url: 解析：0，,
+	filter_url: 'area={{fl.area or "0"}}&sort={{fl.sort or "update"}}&year={{fl.year or "0"}}&category_id={{fl.cateId}}',
 	filter_def: {
 		0:{cateId:'0'},
-		1:{用aaaa编辑jp.js:'1'},
-		2:{档案:'2'},
-		3:{面包屑:'3'},
-		4:{aaaa级:'4'}
+		1:{cateId:'1'},
+		2:{cateId:'2'},
+		3:{cateId:'3'},
+		4:{cateId:'4'}
 	},
-	在: {
+	headers: {
 		'User-Agent': 'jianpian-android/350',
 		'JPAUTH': 'y261ow7kF2dtzlxh1GS9EB8nbTxNmaK/QQIAjctlKiEv'
 	},
-	简易套房套房（js）: 5000,
-	取消更改: 8,
-	play_parse: 提交更改。: 0,
+	timeout: 5000,
+	limit: 8,
+	play_parse: true,
 	play_json: [{
-		编辑: '*',
-		预演'@Referer=www.jianpianapp.com@User-Agent=jianpian-version353'，: {
-			使用GitHub副驾驶代码快55％
-			推荐：: 0
+		re: '*',
+		json: {
+			parse: 0,
+			jx: 0
 		}
 	}],
-	缩进模式: '',
-	空: '@Referer=www.jianpianapp.com@User-Agent=jianpian-version353',
-	制表符 d = [];
-query为空
-让html=按键移动焦点。或者，使用(input)；
-html = JSON.欧洲心脏病学会(html).然后[0].突耳;
-html.移动到页面上的下一个交互元素。(通过 => {
-标题，
-            d.拖拽，({
-选择或粘贴它们。: it.请求,
-作句法分析: it.数据.视频 + 有 + it.为了,
-它.推
-/一级：'json:data；title；path；playlist.百万毫克
+	lazy: '',
+	图片来源: '@Referer=www.jianpianapp.com@User-Agent=jianpian-version353',
+	// 推荐:'json:.video;*;*;*;*',
+	推荐: `js:
+        var d = [];
+        let html = request(input);
+        html = JSON.parse(html).data[0].video;
+        html.forEach(it => {
+            d.push({
+                title: it.title,
+                img: it.path,
+                desc: it.playlist.title + ' ⭐' + it.score,
+                url: it.id
             })
         });
-        路径(d);
-,
-desc
-filter_url:
-音乐播放表.tid = cateObj.标题+'';
-此文件为空。 (cateObj.' ⭐'.编者某偃汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲汲('_clicklink')) {
-cateObj.解析：0， = cateObj.cateId.cateId('_')[0];
-input = HOST + '/api/video/search?key=' + cateObj.cateId + '&page=' + + MY_PAGE;
-		}
-再试一次。 d = [];
-让html=*cateId(input)；
-html = JSON.* https:cateId
-html.标题 rule = {(用aaaa，jp => {
-            d.档案'http://apii.rinhome.com'面包屑({
-aaaa级'/api/tag/hand？code=unknown601193 cf375 db73 d&channel=wandoujia图片来源：
-img: it./推荐：在，：0，
-class_name:懒惰的，图片来源
-过滤：1，.简易套房套房（js）{
-            })
-        });
-        编者按记者说，“一”------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------}，{取消更改html=提交更改。(input)；}，{编辑}，{预演 = JSON.缩进模式(html).空[0].制表符;}，{按键移动焦点。或者，使用}，{html.欧洲心脏病学会(然后 => {}]，{突耳[{标题，}，{img: it.移动到页面上的下一个交互元素。,}，{通过(拖拽，);
-filter_def:
-query为空
-选择或粘贴它们。{
-附加工资=data. 上传你的文件……: it.playlist.title + ' ⭐' + it.score,{我们不支持这种文件类型。}，{再试一次}，{网址：it.GIF、JPEG、JPG、MOV、MP4、PNG、SVG或 WEBM}，{/一级：'json:data；title；path；playlist.附加文档需要对此存储库的写权限。'}，{再试一次}]}，{有GIF、JPEG、JPG、MOV、MP4、PNG、SVG[{我们不支持这种文件类型。}，{cateObj.再试一次 = cateObj.有+''; }，{GIF、JPEG、JPG、MOV、MP4、PNG、SVG或 WEBM}，{再试一次}，{使用一个不空的文件。 (cateObj.这个文件是隐藏的。.再试一次('_clicklink')) {}，{另一份文件。.出了问题，我们无法处理那个文件。 = cateObj.影视电视在你吗./T. 我/fongmi_fongmi_vomakevomakevolk/html（html）.*https：//github. com/fongmi/Release/tree/main/apk；（'_'）[0]；}，{哪里=HOST+'/api/video/search？key='+cateObj.：“”：“black”，++'&PAGE=++MY_PAGE='，\\\\\\\\\\\\\type\\\\\\\\\\\\\\\fongs\type\\\\\\\\\fongs\type++++++++++++makey\\\
-超文本标记语言 = JSON.全部和 mayou wilyou mayoto和mayoto”(html).///mayou/api/term/ad_fenlei？limit=10&page=1:it. class_Url：'0&1&2&3&4'，|it. detailUrl：'/api/node/detail？channel=wandoujia&token=&id=fyid'，//二级详情拼接链接(JSON you stone)。我国“电化皿砚台“中”“中”“中”“中”“中”“中”“中'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''兰的谷下兰的谷下兰的谷下兰的谷下兰的谷下兰的谷下兰的谷下兰的谷下兰的谷下兰的谷下兰的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷|it.[n]：[k]：[v]：[v]：[5]。[n]：[v]：[18]）+[n]：[n]：[n]，[v]：[2]+it。[n]：[01]，[v]：[0]，}，{{：it。[n]：[2023]，[v]：[123]，[123]}，{[n]：[2022]，[v]：[101]}，{[n]：[2021]，[v]：[118]，{n]，[v]：〔2020]}，[v]：〔2019]，[v]：7]
-}).“n”：“2018”，“v”：“2” link=data. “n”：“2017”，“v”：“3” [n]：[v]：[热]=>{}}，{“钥匙”：“年份”，“名称”：“价值”：[{“排序”'[a=cr：'+JSON.返回{'id'：it.'[a=cr：'+'_clicklink'，'name'：it.年化木购买木乃年化木购买木乃年化木皿次皿皿皿皿皿皿皿皿皿次砾（）兰投资电位兰（）兰投资电位谷（年）兰购买的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷下年的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的谷的}）+'/]'+it.“n”：“v”：“2”+'[/a]'}，{“n”：“2023”，“v”：“153”}，{“n”：“2022”，“v”：“101”}，{“n”：“2021”，“v”：“118”}，{“n”：“2020”，“v”：“16”}，{“n”：“2019”，“v”：“7”}，{[2018]，[五]：[2]{}).“n”：“v”：“0”(“n”：“2023”，“v”：“153”);(“2017”，“v”：“3”}，{“n”：“2016”，“v”：“22”}]，{“key”：“sort”，“name”：“排序”，“value”：[{“乘谕乘谕“乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕乘谕93亿个巨兽的巨额财产来源不明财产来源不明财产来源不明财产来源不明财产来源不明财产来源不明财产来源不明财产来源不明财产来源不明财产来源不明财产来源不明财产来源不明财产来源不明财产来源不明财产来源不明财产来源不明财产来源不明财产来源不明财产来源不明财产}，{[n]：[v]：[税]：[评级]}，{“n”：“v”：“update”}]);
-返回链接
+        setResult(d);
+    `,
+	// 一级:'json:data;title;path;playlist.title;id',
+	一级: `js:
+		cateObj.tid = cateObj.tid+'';
+        if (cateObj.tid.endsWith('_clicklink')) {
+            cateObj.tid = cateObj.tid.split('_')[0];
+            input = HOST + '/api/video/search?key=' + cateObj.tid + '&page=' + + MY_PAGE;
         }
-试试{
-让html=filter_url：试试{
-html = JSON.filter_def: {(让html={N]：[2022]，[v]：[101]}，{[n]：[2021]，[v]：[118]}，{[n]：[2020]，[v]：[16]}，{[n]：[2019]，[v]：[7]}，{[2018]，[v]：[2]，{[n]}]+'/]'+it.[2017]，[v]：[3]}，{[n]：[2016]，[v]：[22]]]]，{[key]：[排序]，[排序]：[$]，[value]：[n]：[v]：]：[v](input)；);
-标题：{ node = html.'User-Agent': 'jianpian-android/350',;
-VOD = {
-vod_id: node.谷购买谷的谷购买木乃谷购买的谷购买谷购买的谷购买谷购买的谷购买的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷下家的谷的谷的 = JSON.“n”：“v”，“v”：“0”(html);,
-vod_name: node.“n” node = html.[n]：[v]：[热];+换挡+m,
-vod_pic: node.'区域={叉长度（fl）.或[0]}}&sort={叉长度（fl）. 排序 you“update”}}&year={叉长度（fl）. year__}}&category_Id={叉长度（fl）.cateId}}}，（输入）； = {,
-type_name: node.vod_id: node.超文本标记语言,[0].vod_name: node.使用,,
-vod_year: 控制.vod_pic: node.切换,.type_name: node.突耳[0].按键移动焦点。或者，使用,,
-vod_area: node.vod_year: node.欧洲心脏病学会.然后,.vod_area: node.突耳.移动到页面上的下一个交互元素。,,
-                vod_备注：,
-                vod_actor: getLink(node.演员),
-                vod_director: getLink(node.董事),
-                vod_content:节点.带()
+        var d = [];
+        let html = request(input);
+        html = JSON.parse(html).data;
+        html.forEach(it => {
+            d.push({
+                title: it.title,
+                img: it.thumbnail||it.path,
+                desc: (it.mask || it.playlist.title) + ' ⭐' + it.score,
+                url: it.id
+            })
+        });
+        setResult(d);
+    `,
+	二级: `js:
+        function getLink(data) {
+            let link = data.map(it => {
+                return '[a=cr:' + JSON.stringify({'id':it.name+'_clicklink','name':it.name}) + '/]' + it.name + '[/a]'
+            }).join(', ');
+            return link
+        }
+		try {
+            let html = request(input);
+            html = JSON.parse(html);
+            let node = html.data;
+            VOD = {
+                vod_id: node.id,
+                vod_name: node.title,
+                vod_pic: node.thumbnail,
+                type_name: node.types[0].name,
+                vod_year: node.year.title,
+                vod_area: node.area.title,
+                vod_remarks: node.score,
+                vod_actor: getLink(node.actors),
+                vod_director: getLink(node.directors),
+                vod_content: node.description.strip()
             };
-            如果 (类型 play_url====='undefined') {
-哪里 play_url=''
+            if (typeof play_url === 'undefined') {
+                var play_url = ''
             }
-让 playMap={};
-			如果 (node.have_ftp_ur == 1) {
-playMap["清风"]=节点.new_ftp_list.地图(它 => {
-返回它。标题 +"$" + (/m3u8/.试验(it.统一资源定位系统)? play_url + it.统一资源定位系统 : "tvbox-xg:"+它。统一资源定位系统)
-				}).加入('#');
+            let playMap = {};
+			if (node.have_ftp_ur == 1) {
+				playMap["清风，勤去清缓存"] = node.new_ftp_list.map(it => {
+					return it.title + "$" + (/m3u8/.test(it.url) ? play_url + it.url : "tvbox-xg:" + it.url)
+				}).join('#');
 			}
-			如果 (node.have_m3u8_ur == 1) {
-playMap["清风"]=节点.new_m3u8_list.地图(它 => {
-返回它。标题 +"$" + (/m3u8/.试验(it.统一资源定位系统)? play_url + it.统一资源定位系统 : "tvbox-xg:"+它。统一资源定位系统)
-				}).加入('#');
+			if (node.have_m3u8_ur == 1) {
+				playMap["在线点播普清版"] = node.new_m3u8_list.map(it => {
+					return it.title + "$" + (/m3u8/.test(it.url) ? play_url + it.url : "tvbox-xg:" + it.url)
+				}).join('#');
 			}
-让 playFrom=[];
-让 音乐播放表 =[];
-            Object.键(playMap).为了(键 => {
-                playFrom.附加(键);
-                playList.附加(playMap[键])
+            let playFrom = [];
+            let playList = [];
+            Object.keys(playMap).forEach(key => {
+                playFrom.append(key);
+                playList.append(playMap[key])
             });
-            VOD.vod_play_from= playFrom.加入('$$$');
-            VOD.vod_play_url=播放列表。加入('$$$');
-        } 抓住 (E) {
-            日志("获取二级详情页发生错误:"+ e.消息);
+            VOD.vod_play_from = playFrom.join('$$$');
+            VOD.vod_play_url = playList.join('$$$');
+        } catch (e) {
+            log("获取二级详情页发生错误:" + e.message);
         }
 	`,
-/搜索：'json:data；*；you you；*'
-搜索：js：
-哪里 D=[]；
-让 超文本标记语言 = 请求(输入);
-html=JSON.作句法分析（you mayoto you mayou）；
-html.为了=>{
-d.推{
-标题：它。标题,
-百万毫克：它。略图,
-desc:它
-统一资源定位系统：它。身份证
+	// 搜索:'json:data;*;thumbnail;mask;*',
+	搜索: `js:
+        var d = [];
+        let html = request(input);
+        html = JSON.parse(html).data;
+        html.forEach(it => {
+            d.push({
+                title: it.title,
+                img: it.thumbnail,
+                desc: it.mask + ' ⭐' + it.score,
+                url: it.id
             })
         });
-        setResult(D);
+        setResult(d);
     `,
 }
